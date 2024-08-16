b19b9bfe51dfec697287c3c940ceb1cba95f0d47
이규락 <62545703+leegyurak@users.noreply.github.com>
2024-08-16 23:52:48 +0900

    ADD: allow hosts

--- joatss/settings.py
+++ joatss/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["api-joatss.devgyurak.com"]
 
 
 # Application definition
