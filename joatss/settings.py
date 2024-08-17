204bbc7c8c4a61ee90353fcf5b4d7def5b958be9
이규락 <62545703+leegyurak@users.noreply.github.com>
2024-08-17 23:37:42 +0900

    ADD: flag

--- joatss/settings.py
+++ joatss/settings.py
@@ -23,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ['ENV'] == 'local':
+    DEBUG = True
+    ALLOWED_HOSTS = ["*"]
 
-ALLOWED_HOSTS = ["*"]
+else:
+    DEBUG = False
+    ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
 
 
 # Application definition
